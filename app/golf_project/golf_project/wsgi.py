e91b6dbffb0145c25125945491362fc3790a51af
Paul Nitto <>
2018-04-08 23:49:07 -0400

    aad

--- app/golf_project/golf_project/wsgi.py
+++ app/golf_project/golf_project/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "golf_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 from django.whitenoise import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "golf_project.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
