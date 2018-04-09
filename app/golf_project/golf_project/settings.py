aa1ccd83837c2ba5fe5772ee39a71b326d25ea4d
Paul Nitto <>
2018-04-08 22:21:18 -0400

    fixing settings

--- app/golf_project/golf_project/settings.py
+++ app/golf_project/golf_project/settings.py
@@ -90,8 +90,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
         'golf_app/static',
-    ),
-)
+    )
 
 
 # Internationalization
