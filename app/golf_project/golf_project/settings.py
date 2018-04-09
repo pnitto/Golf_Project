a9f445715a6e2709538dc9192e06e18e1d6a9310
Paul Nitto <>
2018-04-09 00:07:34 -0400

    updated settings

--- app/golf_project/golf_project/settings.py
+++ app/golf_project/golf_project/settings.py
@@ -43,14 +43,14 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'golf_project.urls'
@@ -89,10 +89,9 @@ DATABASES = {
 STATICFILES_DIRS = (
     os.path.join(
         BASE_DIR,
-        'golf_app/static'
+        "static"
     ),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Internationalization
