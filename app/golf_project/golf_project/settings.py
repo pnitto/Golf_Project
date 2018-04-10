448ba92fb29e5f1da0f2aa5bbc5dc952e7af17c1
Paul Nitto <>
2018-04-09 21:53:48 -0400

    settings change

--- app/golf_project/golf_project/settings.py
+++ app/golf_project/golf_project/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#where you want static files to go
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
